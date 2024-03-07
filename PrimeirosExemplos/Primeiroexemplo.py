7030ee770822462f16a2c841e5f73d064e836337
andredickel <andredickel04@gmail.com>
2024-03-06 21:39:39 -0300

    PrimeiroExemplo.py created

---
+++ PrimeirosExemplos/Primeiroexemplo.py
@@ -0,0 +1,20 @@
+# Comando de sáida
+print('Hello')
+
+# Variavel numérica e atribuindo valor
+num = 10
+
+# Imprimindo valor da variavel num
+print (num)
+
+# Trocando o valor da variavel num
+num = -1
+print(num)
+
+# Leitura (comando de entrada) de valor para a variavel nome
+nome = input('Digite seu nome e PRESSIONE ENTER: ')
+print (nome)
+
+# Leitura de valor inteiro para a variavel idade
+idade = int(input('Digite sua idade: '))
+print('Seu nome é', nome, 'Sua idade é',idade)
