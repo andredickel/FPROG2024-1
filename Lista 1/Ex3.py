a903c9abde56a69935636e1534c7cc72bbf5c120
andredickel <andredickel04@gmail.com>
2024-03-13 20:36:35 -0300

    Create Ex3

---
+++ Lista 1/Ex3.py
@@ -0,0 +1,17 @@
+# Exercicio 2 
+print ('Qual é o verdadeiro nome do super-herói Batman?')
+print ('a) Bruce Wayne')
+print ('b) Clark Kent')
+print ('c) Peter Parker')
+print ('d) Tony Stark')
+print ('e) Steve Rogers')
+
+# Váriavel correta
+resposta = 'a'
+
+# Resposta do usuário
+respostadousuario = input('Digite sua resposta: ')
+
+# Mensagem informando a resposta correta
+print('A sua resposta foi',respostadousuario,'\nA resposta correta é',resposta)
+       
