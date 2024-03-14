7b2b14d4806423658828f0c97396c268524faf84
andredickel <andredickel04@gmail.com>
2024-03-13 21:03:38 -0300

    Create Ex4.py

---
+++ Lista 1/Ex4.py
@@ -0,0 +1,19 @@
+pergunta = 'Qual é o verdadeiro nome do super-herói Batman?'
+alternativaA = 'Bruce Wayne'
+alternativaB = 'Clark Kent'
+alternativaC = 'Peter Parker'
+alternativaD = 'Tony Stark'
+alternativaE = 'Steve Rogers'
+
+print(pergunta)
+print('a)',alternativaA)
+print('b)',alternativaB)
+print('c)',alternativaC)
+print('d)',alternativaD)
+print('e)',alternativaE)
+
+respostadousuario = input('Digite sua resposta: ')
+
+respostacorreta = alternativaA
+
+print('A sua resposta foi:',respostadousuario,'\nA resposta correta é:',respostacorreta)
