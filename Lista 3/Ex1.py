a8c93ed73bca38c4d2272af670151927049463bf
andredickel <andredickel04@gmail.com>
2024-03-20 21:53:31 -0300

    Lista 3

---
+++ Lista 3/Ex1.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número inteiro:'))
+n1 = int(input('Digite outro número inteiro:'))
+
+if n / n != 0:
+    print('Erro')
+
+else:
+    print('a')
---
+++ Lista 3/Ex4.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um número inteiro: '))
+
+if numero % 3 == 0:
+    print('O número inteiro informado pelo usuário é divisivel por 3')
+
+else:
+    print('O número inteiro informado pelo usuário não é divisivel por 3')
